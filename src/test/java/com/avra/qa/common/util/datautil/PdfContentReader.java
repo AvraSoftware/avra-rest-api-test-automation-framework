1eaa59f83b4324f80cfbaac2adcd2f34d9941c0e
Maciej Dobrzanski <maciej.dobrzanski@avra.pl>
2024-09-03 14:40:08 +0200

    dependencies up

--- src/test/java/com/avra/qa/common/util/datautil/PdfContentReader.java
+++ src/test/java/com/avra/qa/common/util/datautil/PdfContentReader.java
@@ -4,18 +4,16 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.springframework.stereotype.Component;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
+
+import static org.apache.pdfbox.Loader.loadPDF;
 
 @Component
 public class PdfContentReader {
 
     public String extractPdfContent(byte[] pdf) throws IOException {
-        PDDocument document = PDDocument.load(new ByteArrayInputStream(pdf));
-        try {
+        try (PDDocument document = loadPDF(pdf)) {
             return new PDFTextStripper().getText(document);
-        } finally {
-            document.close();
         }
     }
 }
